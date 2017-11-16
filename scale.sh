52ca380c886282d41d867e0cebaa35f2266c9120
Michal Srb <michal@redhat.com>
2017-11-16 23:18:29 +0100

    Rewrite scaling logic

--- scale.sh
+++ scale.sh
@@ -1,33 +1,21 @@
 #!/usr/bin/bash -e
 
-function scale_to_default { 
-    oc -n $OC_PROJECT scale --replicas=$DEFAULT_REPLICAS dc $DC_NAME
-}
-
 set -x
 oc whoami
 oc project
 set +x
+
+queue_name=${DEPLOYMENT_PREFIX}_${SQS_QUEUE_NAME}
+
 while true; do
     # get number of messages in the given queue
-    number_of_messages=$(./sqs_status.py -q ${DEPLOYMENT_PREFIX}_${SQS_QUEUE_NAME})
+    msg_count replicas <<< $(./sqs_status.py -q ${queue_name})
 
-    echo "Number of messages in queue: $number_of_messages"
-
-    #run scale based on number of messages
-    if (( NUMBER_OF_MESSAGES > 10000 )); then
-        oc -n $OC_PROJECT scale --replicas=$MAX_REPLICAS dc $DC_NAME
-
-    elif (( NUMBER_OF_MESSAGES < 5000 )); then
-        scale_to_default
-
-    elif (( NUMBER_OF_MESSAGES < 1000 )); then
-        oc -n $OC_PROJECT scale --replicas=3 dc $DC_NAME
-
-    elif (( NUMBER_OF_MESSAGES < 500 )); then
-        oc -n $OC_PROJECT scale --replicas=2 dc $DC_NAME
-    fi
+    echo "[$(date -u)] Number of messages in ${queue_name} is ${msg_count}. Replicas needed: ${replicas}."
+    set -x
+    oc -n $OC_PROJECT scale --replicas=${replicas} dc $DC_NAME
+    set +x
 
     date -u +%s > /var/lib/f8a-scaler/liveness
-    sleep 1m
+    sleep 5m
 done
