064606a83bd69dfe46ca85ce0fa92e5e692cf761
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-18 15:07:55 +0200

    Updated runtest.sh script

--- qa/runtest.sh
+++ qa/runtest.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+pushd "${SCRIPT_DIR}/.." > /dev/null
+
 set -ex
 
 check_python_version() {
@@ -9,3 +13,5 @@ check_python_version() {
 check_python_version
 
 python3 -m doctest sqs_status.py
+
+popd > /dev/null
